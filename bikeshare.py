6145bec62d38d827717b9a4a9a2e3c14610cbd02
Wen-Xiao <wxiao856@gmail.com>
2024-08-17 19:05:13 -0600

    update the get_filters() function

--- bikeshare.py
+++ bikeshare.py
@@ -6,6 +6,15 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+def get_user_input(prompt, valid_options):
+    """Handles input validation for user choices."""
+    while True:
+        user_input = input(prompt).lower()
+        if user_input in valid_options:
+            return user_input
+        else:
+            print("Invalid input. Please enter a valid option.")
+
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
@@ -17,29 +26,13 @@ def get_filters():
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-    while True:
-        city = input("Would you like to see data for Chicago, New York City, or Washington?\n").lower()
-        if city in CITY_DATA:
-            break
-        else:
-            print("Invalid input. Please enter a valid city name.")
-
+    city = get_user_input("Would you like to see data for Chicago, New York City, or Washington?\n", CITY_DATA.keys())
 
     # TO DO: get user input for month (all, january, february, ... , june)
-    while True:
-        month = input("Which month? (all, january, february, march, april, may, june)\n").lower()
-        if month in ['all', 'january', 'february', 'march', 'april', 'may', 'june']:
-            break
-        else:
-            print("Invalid input. Please enter a valid month name.")
+    month = get_user_input("Which month? (all, january, february, march, april, may, june)\n", ['all', 'january', 'february', 'march', 'april', 'may', 'june'])
 
     # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
-    while True:
-        day = input("Which day? (all, monday, tuesday, wednesday, thursday, friday, saturday, sunday)\n").lower()
-        if day in ['all', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']:
-            break
-        else:
-            print("Invalid input. Please enter a valid day of the week.")
+    day = get_user_input("Which day? (all, monday, tuesday, wednesday, thursday, friday, saturday, sunday)\n", ['all', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'])
 
     print('-'*40)
     return city, month, day
